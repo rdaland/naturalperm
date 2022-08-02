c51d4d271118c7db1a20422926de033abc3c33ea
Robert Daland <rdaland@Roberts-MacBook-Air.local>
2022-08-02 13:32:05 -0700

    convert int <-> prime factorization

--- naturalperm/naturalperm.py
+++ naturalperm/naturalperm.py
@@ -1,4 +1,5 @@
 from typing import List
+from math import prod
 from primefac import primefac, primegen
 
 def int_to_binary_string(n: int) -> str:
@@ -9,7 +10,17 @@ def binary_string_to_int(bs: str) -> int:
     return int(bs, 2)
 
 
-SOME_BIG_INT = 600851475143
+def int_to_prime_factorization(n: int) -> List[int]:
+    return sorted(primefac(n))
+
+
+def prime_factorization_to_int(plist: List[int]) -> int:
+    return prod(plist)
+
+
+#def encode_primefac_as_binary_string()
+
+
 def main():
     x = binary_string_to_int('100')
     print(x)
--- tests/test_naturalperm.py
+++ tests/test_naturalperm.py
@@ -1,25 +1,18 @@
 import pytest
 from naturalperm import __version__
 from naturalperm.naturalperm import (
-    bit_to_int,
     int_to_binary_string,
     binary_string_to_int,
+    int_to_prime_factorization,
+    prime_factorization_to_int,
 )
+
+SOME_BIGINT = 600851475143
+BIGINT_FACTORS = [71, 839, 1471, 6857]
 
 # illustrate module paths
 def test_version():
     assert __version__ == '0.1.0'
-
-
-def test_bit_to_int():
-    assert bit_to_int('0') == 0
-    assert bit_to_int('1') == 1
-    with pytest.raises(ValueError):
-        bit_to_int(0)
-    with pytest.raises(ValueError):
-        bit_to_int('00')
-    with pytest.raises(ValueError):
-        bit_to_int('2')
 
 
 def test_int_to_binary_string():
@@ -33,4 +26,12 @@ def test_binary_string_to_int():
     assert binary_string_to_int('0100') == 4
     with pytest.raises(ValueError):
         binary_string_to_int('1.1')
+
+
+def test_int_to_prime_factorization():
+    assert int_to_prime_factorization(SOME_BIGINT) == BIGINT_FACTORS
+
+
+def test_prime_factorization_to_int():
+    assert prime_factorization_to_int(BIGINT_FACTORS) == SOME_BIGINT
 
