61635b2a9311e168b9f879374f0b945686fa5121
Robert Daland <rdaland@Roberts-MacBook-Air.local>
2022-08-13 15:17:20 +0100

    refactor to match writeup: benc/bdec, penc/pdec

--- tests/test_naturalperm.py
+++ tests/test_naturalperm.py
@@ -1,12 +1,11 @@
 import pytest
+import itertools
 from naturalperm import __version__
 from naturalperm.naturalperm import (
-    int_to_binary_string,
-    binary_string_to_int,
-    int_to_prime_factorization,
-    prime_factorization_to_int,
-    encode_primefac_as_binary_string,
-    decode_binary_string_as_primefac,
+    benc,
+    bdec,
+    penc,
+    pdec,
     natperm,
     natperminv,
 )
@@ -18,51 +17,73 @@ BIGINT_FACTORS = [71, 839, 1471, 6857]
 def test_version():
     assert __version__ == '0.1.0'
 
+def strip_leading_zeros(binstr: str) -> str:
+    if len(binstr) == 1:
+        return binstr
+    try:
+        return binstr[binstr.index('1'):]
+    except ValueError:
+        return '0'
 
-def test_int_to_binary_string():
-    assert int_to_binary_string(4) == '100'
-    with pytest.raises(ValueError):
-        int_to_binary_string('4')
+def test_strip_leading_zeros():
+    assert strip_leading_zeros('0') == '0'
+    assert strip_leading_zeros('01') == '1'
+    assert strip_leading_zeros('00') == '0'
+    assert strip_leading_zeros('00000') == '0'
+    
 
+class TestBinaryTransformer:
+    def test_benc(self):
+        assert benc(1) == '0'
+        assert benc(2) == '1'
+        assert benc(3) == '10'
 
-def test_binary_string_to_int():
-    assert binary_string_to_int('100') == 4
-    assert binary_string_to_int('0100') == 4
-    with pytest.raises(ValueError):
-        binary_string_to_int('1.1')
+    def test_bdec(self):
+        assert bdec('0') == 1
+        assert bdec('1') == 2
+        assert bdec('10') == 3
 
-
-def test_int_to_prime_factorization():
-    assert int_to_prime_factorization(SOME_BIGINT) == BIGINT_FACTORS
-
-
-def test_prime_factorization_to_int():
-    assert prime_factorization_to_int(BIGINT_FACTORS) == SOME_BIGINT
-
-
-def test_encode_primefac_as_binary_string():
-    assert encode_primefac_as_binary_string([]) == '0'
-    assert encode_primefac_as_binary_string([2, 2, 3]) == '1011'
-
-
-def test_decode_binary_string_as_primefac():
-    assert decode_binary_string_as_primefac('0') == []
-    assert decode_binary_string_as_primefac('1011') == [2, 2, 3]
-
-
-def test_natperm__basic():
-    assert natperm(1) == 1
-    with pytest.raises(ValueError):
-        natperm(0)
+    def test_benc_and_bdec_are_inverses(self):
+        for i in range(1, 100 + 1):
+            assert bdec(benc(i)) == i
+        for bintuple in itertools.product('01', repeat=8):
+            binstr = strip_leading_zeros(''.join(bintuple))
+            assert benc(bdec(binstr)) == binstr
 
 
-def test_natperminv__basic():
-    assert natperminv(1) == 1
-    with pytest.raises(ValueError):
-        natperminv(0)
+class TestPrimeTransformer:
+    def test_penc(self):
+        with pytest.raises(ValueError):
+            penc(0)
+        assert penc(1) == '0'
+        assert penc(2) == '1'
+        assert penc(3) == '10'
+
+    def test_pdec(self):
+        assert pdec('0') == 1
+        assert pdec('1') == 2
+        assert pdec('10') == 3
+
+    def test_penc_and_pdec_are_inverses(self):
+        for i in range(1, 100 + 1):
+            assert pdec(penc(i)) == i
+        for primtuple in itertools.product('01', repeat=8):
+            primstr = strip_leading_zeros(''.join(primtuple))
+            assert penc(pdec(primstr)) == primstr
 
 
-def test_natperm_and_natperminv_are_inverses():
-    for i in range(1, 100):
-        assert natperminv(natperm(i)) == i
-        assert natperminv(natperm(i)) == i
+class TestNaturalPermutation:
+    def test_natperm__basic(self):
+        assert natperm(1) == 1
+        with pytest.raises(ValueError):
+            natperm(0)
+
+    def test_natperminv__basic(self):
+        assert natperminv(1) == 1
+        with pytest.raises(ValueError):
+            natperminv(0)
+
+    def test_natperm_and_natperminv_are_inverses(self):
+        for i in range(1, 100 + 1):
+            assert natperminv(natperm(i)) == i
+            assert natperminv(natperm(i)) == i
